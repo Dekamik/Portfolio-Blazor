96a47f31afd444e4202d713a42b87c384b4d8b5c
Dennis von Bargen <dennis@vonbargen.se>
2020-08-29 11:35:04 +0200

    Collapse not working, need to find a way for bootstrap to work properly

---
+++ Portfolio/Shared/LinqExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Portfolio.Shared
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> source)
+        {
+            return source.Select((item, index) => (item, index));
+        }
+    }
+}
---
+++ Portfolio/Shared/ProjectDescription.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Portfolio.Shared
+{
+    public class ProjectDescription
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+    }
+}
